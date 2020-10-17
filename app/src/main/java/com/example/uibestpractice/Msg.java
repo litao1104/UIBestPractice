3a5ab0c50ab0cf85ca0b5e9c08e5f35650fa54ad
litao1104 <litao1104@gmail.com>
2020-10-17 12:26:59 +0800

    comlepte UI implementation

--- app/src/main/java/com/example/uibestpractice/Msg.java
+++ app/src/main/java/com/example/uibestpractice/Msg.java
@@ -2,9 +2,11 @@ package com.example.uibestpractice;
 
 public class Msg {
     public static final int TYPE_RECEIVED = 0;
+
     public static final int TYPE_SENT= 1;
 
     private String content;
+
     private int type;
 
     public Msg(String content, int type) {
